f99f161b12ff06357818472369e590f4e98d3648
JJCone <jackson.j.cone@gmail.com>
2022-08-09 13:15:20 -0600

    Updated Analyses and kernel plot calls
    Add a stimType info to individual and grand kernel calls to differentiate between Gabor, luminance, and offset conditions.

--- Analysis Code/grandAverageKernels.m
+++ Analysis Code/grandAverageKernels.m
@@ -1,25 +1,51 @@
-function grandAverageKernels(brainArea)
+function grandAverageKernels(brainArea, stimType)
 % Plot all the kernels for the grand average across selected sessions
 
-% brainArea input is a string: 
-% 'SC' for superior colliculus  
+% brainArea input is a string:
+% 'SC' for superior colliculus
 % 'V1' for visual cortex
 
+% stimType input is also a string:
+% 'Lum' for luminance patch
+% 'Gab' for Gabor patch
+% 'Off' for Offset Control (misaligned)
+
+% if not, throw errors
+assert(ischar(brainArea),'brainArea must be text: SC or V1');
+assert(ischar(stimType),'stimType must be text: Lum, or Gab or Off');
+ 
+ 
   [~, analysisDirName, tableName] = whichData();
   load([analysisDirName, tableName], 'T');
   limits = setLimits('All');
   limits.rampMS = 0;
  
   if strcmp(brainArea,'SC')
-      % Full Kernel For Aligned Luminance Patch
-      limits.animal = {'1458', '1548', '1674', '1675', '1902', '1905'};
-      % VGATs
-      %limits.animal = {'2057', '2058', '2063'};
       
-      % Control Kernel For Offset Luminance Patch
-      % limits.animal = {'1674', '1675', '1902'};
+      if strcmp(stimType,'Lum')
+          % Full Kernel For Aligned Luminance Patch
+          limits.animal = {'1458', '1548', '1674', '1675', '1902', '2057', '2058', '2063', '2169', '2236'};
+      elseif strcmp(stimType,'Gab')
+          % Kernel for Aligned Gabor Patch
+          limits.animal = {'1548', '1674', '2057', '2058', '2063', '2169'};
+      elseif strcmp(stimType,'Off')
+          % Control Kernel For Offset Luminance Patch
+          limits.animal = {'1674', '1675', '1902', '2057', '2063'};
+      end
+      
+      
   elseif strcmp(brainArea,'V1')
-      limits.animal = {'1951', '1956', '1960', '1995', '2015', '2016', '2018', '2080'};
+      if strcmp(stimType,'Lum')
+          % V1 Luminance
+          limits.animal = {'1960','2015','2083','2126'};
+      elseif strcmp(stimType,'Gab')
+          % V1 Gabor
+          limits.animal = {'1960', '2015', '2016', '2083', '2126', '2207', '2220', '2221'};
+      elseif strcmp(stimType,'Off')
+          % V1 Offset
+          limits.animal = {'2016','2083', '2126', '2220'};
+      end
+      
   end
   
   
--- Analysis Code/individualKernels.m
+++ Analysis Code/individualKernels.m
@@ -1,10 +1,20 @@
-function individualKernels(brainArea)
+function individualKernels(brainArea, stimType)
 % Plot all the kernels for the grand average across selected sessions, once for steps and once for ramps
 
   % brainArea input is a string:
   % 'SC' for superior colliculus
   % 'V1' for visual cortex
 
+  % stimType input is also a string:
+  % 'Lum' for luminance patch
+  % 'Gab' for Gabor patch
+  % 'Off' for Offset Control (misaligned)
+  
+  % if not, throw errors
+  assert(ischar(brainArea),'brainArea must be text: SC or V1');
+  assert(ischar(stimType),'stimType must be text: Lum, or Gab or Off');
+  
+  
   rampMS = 0;
   
   [~, analysisDataDir, tableName] = whichData();
@@ -14,20 +24,35 @@ function individualKernels(brainArea)
   limits.rampMS = rampMS;
   
   if strcmp(brainArea,'SC')
-      % Full Kernel For Aligned Luminance Patch
-      % animals = {'1458', '1548', '1634',  '1674', '1675', '1902', '1905'};
-      animals = {'2057', '2058', '2063'};
-      % Control Kernel For Offset Luminance Patch
-      % animals = {'1674', '1675', '1902'};
-      % animals = {'1634'};
       
-      % Full Kernel For Aligned Gabor Patch
-      % animals = {'1458', '1548', '1674'};
-      % animals = {'2054', '2058', '2060', '2063'};
+      if strcmp(stimType,'Lum')
+          % Full Kernel For Aligned Luminance Patch
+          animals = {'1458', '1548', '1674', '1675', '1902', '2057', '2058', '2063', '2169', '2236'};
+          % animals = {'2169', '2236'};
+      elseif strcmp(stimType,'Gab')
+          % Full Kernel For Aligned Gabor Patch
+          animals = {'1548', '1674', '2057', '2058', '2063', '2169'};
+      elseif strcmp(stimType, 'Off')
+          % Control Kernel For Offset Luminance Patch
+          animals = {'1674', '1675', '1902', '2057', '2063', '2236'};
+          % animals = {'2057', '2063', '2236'};
+          
+      end
+      
       
   elseif strcmp(brainArea,'V1')
-      animals = {'1956', '1960', '1995', '1996', '1998', '2015', '2016', '2017', '2018', '2078', '2080', '2083', '2086', '2126', '2128'};
-      % animals = {'1958', '1960', '1995', '1998'};
+      
+      if strcmp(stimType,'Lum')
+          % V1 Luminance
+          animals = {'1960','2015','2083','2126'};
+      elseif strcmp(stimType,'Gab')
+          % V1 Gabor
+          animals = {'1960', '2015', '2016', '2083', '2126', '2207', '2220', '2221'};
+      elseif strcmp(stimType,'Off')
+          % V1 Offset
+          animals = {'2016','2083', '2126', '2220'};
+          
+      end
   end
   
  
